d751b996a26e01c067890be1ce6fb65536b28e1a
zhaozg <zhaozg@gmail.com>
2016-01-09 19:41:52 +0800

    update amalgamation.c

--- amalgamation.c
+++ amalgamation.c
@@ -1,4 +1,11 @@
+#ifndef SQLITE_HAS_CODEC
+#define SQLITE_HAS_CODEC 1
+#endif
+#ifndef SQLITE_TEMP_STORE
+#define SQLITE_TEMP_STORE 2
+#endif
+
 #include "src/sqlite3.c"
-#include "ext/cipher/crypto_impl.c"
 #include "ext/cipher/crypto.c"
+#include "ext/cipher/crypto_impl.c"
 #include "ext/cipher/crypto_openssl.c"
